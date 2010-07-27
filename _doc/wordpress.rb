044e5edb99f5f65a59df0cb91252d2228a30f596
Andy Rossmeissl <andy@rossmeissl.net>
2010-07-27 14:45:09 -0500

    My tweaked convertor

---
+++ _doc/wordpress.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'sequel'
+require 'fileutils'
+require 'yaml'
+
+# NOTE: This converter requires Sequel and the MySQL gems.
+# The MySQL gem can be difficult to install on OS X. Once you have MySQL
+# installed, running the following commands should work:
+# $ sudo gem install sequel
+# $ sudo gem install mysql -- --with-mysql-config=/usr/local/mysql/bin/mysql_config
+
+module Jekyll
+  module WordPress
+    
+    CATS = { '134' => 'company', '137' => 'science', '140' => 'technology'}
+
+    # Reads a MySQL database via Sequel and creates a post file for each
+    # post in wp_posts that has post_status = 'publish'.
+    # This restriction is made because 'draft' posts are not guaranteed to
+    # have valid dates.
+    QUERY = "SELECT * FROM wp_jowi63_posts p JOIN wp_jowi63_term_relationships r ON p.ID = r.object_id JOIN wp_jowi63_term_taxonomy tt ON r.term_taxonomy_id = tt.term_taxonomy_id JOIN wp_jowi63_term_taxonomy t ON tt.term_id = t.term_id WHERE t.term_id IN(#{CATS.keys.join ','}) AND p.post_status='publish' AND p.post_type='post'"
+
+    def self.process(dbname, user, pass, host = 'localhost')
+      db = Sequel.mysql(dbname, :user => user, :password => pass, :host => host)
+
+      FileUtils.mkdir_p "_posts"
+
+      db[QUERY].each do |post|
+        # Get required fields and construct Jekyll compatible name
+        title = post[:post_title]
+        slug = post[:post_name]
+        date = post[:post_date]
+        content = post[:post_content]
+        cat = CATS[post[:term_id].to_s]
+        name = "%02d-%02d-%02d-%s.markdown" % [date.year, date.month, date.day,
+                                               slug]
+
+        # Get the relevant fields as a hash, delete empty fields and convert
+        # to YAML for the header
+        data = {
+           'layout' => 'post',
+           'title' => title.to_s,
+           'excerpt' => post[:post_excerpt].to_s,
+           'wordpress_id' => post[:ID],
+           'cats' => cat,
+           'author' => 'bessie',
+           'wordpress_url' => post[:guid]
+         }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
+
+        # Write out the data and content to file
+        File.open("_posts/#{name}", "w") do |f|
+          f.puts data
+          f.puts "---"
+          f.puts content
+        end
+      end
+
+    end
+  end
+end
+
